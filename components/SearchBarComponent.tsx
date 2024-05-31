db182a3824e84a38309b2315d981e8c69c430f9d
Stephane ANDRE <stephane.andre85@gmail.com>
2024-05-31 19:35:40 +0200

    translation

--- components/SearchBarComponent.tsx
+++ components/SearchBarComponent.tsx
@@ -43,9 +43,9 @@ export function SearchBarModelComponent() {
             <DialogTrigger asChild>
                 <div className="rounded-full py-2 px-5 border flex items-center cursor-pointer">
                     <div className="flex h-full divide-x font-medium">
-                        <p className="px-4">Où tu veux</p>
-                        <p className="px-4">Quand tu veux</p>
-                        <p className="px-4">Avec qui tu veux</p>
+                        <p className="px-4">Oussa?</p>
+                        <p className="px-4">Kansa?</p>
+                        <p className="px-4">ek ki?</p>
                     </div>
                     <Search className="bg-primary text-white p-1 h-8 w-8 rounded-full" />
                 </div>
